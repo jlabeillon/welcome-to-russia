724e6be4617c65cdff24022fa3cf33aea5561b6f
Jean-Luc Abeillon <jlabeillon@hotmail.fr>
2017-10-06 14:57:53 +0200

    JSON + FOSUSER ok

--- project/src/AppBundle/Controller/TestController.php
+++ project/src/AppBundle/Controller/TestController.php
@@ -8,10 +8,6 @@ use AppBundle\Entity\Restaurant;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Serializer\Serializer;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\HttpFoundation\Response;
 use Doctrine\ORM\EntityManagerInterface;
 
@@ -23,13 +19,13 @@ class TestController extends Controller
      */
     public function testCityAction()
     {
-        $encoders = array(new XmlEncoder(), new JsonEncoder());
-        $normalizers = array(new ObjectNormalizer());
-        $serializer = new Serializer($normalizers, $encoders);
 
+
+
+        $serializer = $this->container->get('jms_serializer');
         $citytest = $this->getDoctrine()->getRepository(City::class)->find(1);
-
         $jsonContent = $serializer->serialize($citytest, 'json');
+
 
         return new Response($jsonContent);
 
